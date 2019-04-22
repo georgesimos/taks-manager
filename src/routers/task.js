20f2bc2a2a8cef5f21019c1957ed44b6558062f7
George <admin@georgesimos.com>
2019-04-22 23:35:28 +0300

    seperating Route files

---
+++ src/routers/task.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const Task = require('../models/task')
+const router = new express.Router()
+
+router.post('/tasks', async (req, res) => {
+    const task = new Task(req.body)
+
+    try {
+        await task.save()
+        res.status(201).send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.get('/tasks', async (req, res) => {
+
+    try {
+        const tasks = await Task.find({})
+        res.send(tasks)
+    } catch (e) {
+        res.sendStatus(400)
+    }
+})
+
+router.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const task = await Task.findById(_id)
+        if (!task) {
+            return res.send(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.sendStatus(400)
+    }
+})
+
+router.patch('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['title', 'description', 'completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) return res.status(400).send({ error: 'Invalid updates!' })
+
+    try {
+        const task = await Task.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true })
+        if (!task) return res.sendStatus(404)
+        return res.send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.delete('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const task = await Task.findByIdAndDelete(_id)
+        if (!task) return res.sendStatus(404)
+
+        res.send(task)
+    } catch (e) {
+        res.sendStatus(400)
+    }
+})
+
+module.exports = router
