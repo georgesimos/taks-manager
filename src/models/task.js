882925a56d6f15edf9ead0087cd08be99b659318
George <admin@georgesimos.com>
2019-04-22 21:08:12 +0300

    Folder Structure, POST user and task, GET user and task

---
+++ src/models/task.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+
+const Task = mongoose.model('Task', {
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+})
+
+module.exports = Task
