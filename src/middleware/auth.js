37650b9a46856aeb95bb53fc02077bd9a9905cd2
George <admin@georgesimos.com>
2019-04-24 20:59:14 +0300

    Logout a user and logout all

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -7,10 +7,11 @@ const auth = async (req, res, next) => {
         const decoded = jwt.verify(token, 'mySecret')
         const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
         if (!user) throw new Error()
+        req.token = token
         req.user = user
         next()
     } catch (e) {
-        res.status(401).send({ eroor: 'Please authenticate.' })
+        res.status(401).send({ error: 'Please authenticate.' })
     }
 }
 
